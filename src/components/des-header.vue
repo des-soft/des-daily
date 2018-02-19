ce15306cef2c32f8bb68ecb6436c94550ae50ed3
eczn <1975845422@qq.com>
2018-02-19 22:21:11 +0800

    [+] 可拖动的 des-header

--- src/components/des-header.vue
+++ src/components/des-header.vue
@@ -1,21 +1,81 @@
 <template>
-    <div class="des-header color-main-bg">
+    <div @mousedown="down"
+         @mouseup="up"
+         @mouseleave="leave"
+        class="des-header color-main-bg">
         Des Daily
-        <font-awesome-icon class="to-right" :icon="['fa', 'times']" />
+        <font-awesome-icon @click="close"
+            class="to-right" :icon="['fa', 'times']" />
     </div>
 </template>
 
 <script>
 // import faTimes from '@fortawesome/fontawesome-free-solid/faTimes'
 
+let { remote } = $require('electron'); 
 
+
+let win = remote.getCurrentWindow(); 
+
+window.win = win; 
 
 export default {
-  computed: {
-      icon(){
-          return ''
-      }
-  }
+    data(){
+        return {
+            id: Date.now(), 
+            isDown: false,
+            first: {
+                screenX: 0, 
+                screenY: 0
+            },
+            win_pos: [0, 0]
+        }
+    },
+    created(){
+        let id = this.id; 
+        this.$d_bus.$on(`d-drag-move-${id}`, e => this.move(e)); 
+        this.$d_bus.$emit('d-resizer-reg', this); 
+    }, 
+    methods: {
+        move(e){
+            if (!this.isDown) return; 
+
+            let { screenX, screenY } = this.first; 
+
+            // let delta = {
+            //     x: e.clientX - clientX, 
+            //     y: e.clientY - clientY
+            // }
+            let [x, y] = this.win_pos; 
+            
+            
+            console.log(x, y); 
+            win.setPosition(
+                x + (e.screenX - screenX),
+                y + (e.screenY - screenY)
+            ); 
+
+            // delta
+        }, 
+        down(e){
+            this.isDown = true;
+
+            this.win_pos = win.getPosition(); 
+            this.first.screenX = e.screenX; 
+            this.first.screenY = e.screenY; 
+        }, 
+        close(e){
+            
+
+            // win.close(); 
+        },
+        leave(e){
+            this.isDown = false; 
+        },
+        up(){
+            this.isDown = false; 
+        }
+    }
 }
 </script>
 

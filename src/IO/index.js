a883dcb90d0111b9449d2bea09b21db3f838d882
eczn <1975845422@qq.com>
2018-02-20 02:54:57 +0800

    [~] rename to `io`

---
+++ src/IO/index.js
@@ -0,0 +1,24 @@
+const configer = require('./configer'); 
+
+configer.init(); 
+
+const gitter = require('./gitter'); 
+
+const qnx = require('./qnx'); 
+
+let qiniu_config = configer.data.qiniu; 
+
+qnx.init(
+    qiniu_config.ak,
+    qiniu_config.sk,
+    qiniu_config.domain,
+    qiniu_config.bucket
+); 
+
+let io = {
+    configer,
+    gitter,
+    qnx
+}
+
+module.exports = store;

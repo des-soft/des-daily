fc8906aa2444de2366f34d537a1bd2d1f197ed1b
eczn <1975845422@qq.com>
2018-02-19 19:34:59 +0800

    [~] width and height of brwoser size

--- main.js
+++ main.js
@@ -13,8 +13,8 @@ let mainWindow;
 
 function createWindow(){
     mainWindow = new BrowserWindow({
-        width: 800,
-        height: 600, 
+        width: 1200,
+        height: 800, 
         // frame: false,
         // transparent: true,
         // webPreferences: { experimentalFeatures: true }

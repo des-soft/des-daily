a6fc7dd93a1b019e31659cfe748b1216a3d54b91
eczn <1975845422@qq.com>
2018-02-24 22:54:13 +0800

    [+] DPool map

---
+++ src/daily-core/Query/index.js
@@ -0,0 +1,20 @@
+import $bridge from '@/daily-core/Bridge/client.js'; 
+
+let Q = {}; 
+
+/**
+ * @description 定义 filter 
+ * @param { String   }  name    filter 名字
+ * @param { Function }  filter  定义
+ */
+Q.define = (name, filter) => {
+    Q[name] = () => {
+        return $bridge.req(
+            'DPool/collector'
+        ).then(filter)
+    }
+
+    return Q; 
+}
+
+export default Q; 

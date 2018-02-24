f8ce89d77b013d6776b1d5b9d9609d300cff220f
eczn <1975845422@qq.com>
2018-02-24 21:58:00 +0800

    [F] focus bug

--- src/components/des-calendar-panel.vue
+++ src/components/des-calendar-panel.vue
@@ -1,31 +1,31 @@
 <template>
-    <div class="des-calendar-panel">
-        <div class="info">
-          <span class="info-year">
-            <input type="text" :value="curYear" v-show="yearInputing" class="info-input" ref="yearInput" @change="changeCur('year',arguments[0].target.value)">
-            <span @dblclick="dblclick('year')" v-show="!yearInputing">{{curYear}}</span>
-          </span> 年 
-          <span v-show="curMonth">
-            <span class="info-month" v-show="curMonth">
-              <input type="text" :value="curMonth" v-show="monthInputing" class="info-input" ref="monthInput" @change="changeCur('month',arguments[0].target.value)">
-              <span @dblclick="dblclick('month')" v-show="!monthInputing">{{curMonth}}</span>
-            </span> 月
-          </span>
-          <div class="control-bar">
-            <des-button @click="overview" :disabled="!curMonth">
-              <font-awesome-icon :icon="['fa', 'th']"  />
-            </des-button>
-            <des-button @click="prev" :disabled="curMonth && curMonth<=1 || !curMonth && curYear<=2018">
-              <font-awesome-icon :icon="['fa', 'angle-left']"  />
-            </des-button>
-            <des-button @click="next" :disabled="curMonth && curMonth>=12">
-              <font-awesome-icon :icon="['fa', 'angle-right']"  />
-            </des-button>
-          </div>
-        </div>
-        <des-calendar :year="curYear" :month="curMonth" size="large" v-if="curMonth"></des-calendar>
-        <des-calendar-overview :year="curYear" v-else @click="chooseMonth(arguments[0])"></des-calendar-overview>
+  <div class="des-calendar-panel">
+    <div class="info">
+      <span class="info-year">
+        <input type="text" :value="curYear" v-show="yearInputing" class="info-input" ref="yearInput" @change="changeCur('year',arguments[0].target.value)">
+        <span @dblclick="dblclick('year')" v-show="!yearInputing">{{curYear}}</span>
+      </span> 年 
+      <span v-show="curMonth">
+        <span class="info-month" v-show="curMonth">
+          <input type="text" :value="curMonth" v-show="monthInputing" class="info-input" ref="monthInput" @change="changeCur('month',arguments[0].target.value)">
+          <span @dblclick="dblclick('month')" v-show="!monthInputing">{{curMonth}}</span>
+        </span> 月
+      </span>
+      <div class="control-bar">
+        <des-button @click="overview" :disabled="!curMonth">
+          <font-awesome-icon :icon="['fa', 'th']"  />
+        </des-button>
+        <des-button @click="prev" :disabled="curMonth && curMonth<=1 || !curMonth && curYear<=2018">
+          <font-awesome-icon :icon="['fa', 'angle-left']"  />
+        </des-button>
+        <des-button @click="next" :disabled="curMonth && curMonth>=12">
+          <font-awesome-icon :icon="['fa', 'angle-right']"  />
+        </des-button>
+      </div>
     </div>
+    <des-calendar :year="curYear" :month="curMonth" size="large" v-if="curMonth"></des-calendar>
+    <des-calendar-overview :year="curYear" v-else @click="chooseMonth(arguments[0])"></des-calendar-overview>
+  </div>
 </template>
 
 <script>
@@ -33,6 +33,8 @@ import moment from "moment";
 import DesCalendarOverview from "./des-calendar-overview";
 import DesCalendar from "./des-calendar";
 import DesButton from "./des-button";
+import Vue from 'vue'
+
 export default {
   components: {
     DesCalendarOverview,
@@ -70,10 +72,14 @@ export default {
     dblclick(type) {
       if(type == 'year'){
         this.yearInputing = true;
-        this.$refs.yearInput.focus();
+        Vue.nextTick(() => {
+          this.$refs.yearInput.focus();
+        }); 
       }else{
         this.monthInputing = true;
-        this.$refs.monthInput.focus();
+        Vue.nextTick(() => {
+          this.$refs.monthInput.focus();
+        });
       }
     },
     changeCur(type,value){

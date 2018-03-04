505cf9570e9f09daf6cbfd251503faa9163d68c2
eczn <1975845422@qq.com>
2018-03-04 23:22:58 +0800

    [+] 错误警告

--- src/components/Editor/saveFile.js
+++ src/components/Editor/saveFile.js
@@ -1,6 +1,8 @@
 let fs = $require('fs'); 
 
 export default function(path, data){
+    if (!path) return Promise.reject('saveFile Warnning, Path Not Found'); 
+
     return new Promise((res, rej) => {
         fs.writeFile(path, data, (err, ok) => {
             if (err) {
